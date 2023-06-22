460a17b12e8048d96948ed9631bc1ed6a4df7e33
fishkings <kcjerim1234567@gmail.com>
2023-06-22 16:55:34 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,38 +1,37 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-    searchInputEl.focus();
-})
-
-searchInputEl.addEventListener('focus', function () {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-})
-
-searchInputEl.addEventListener('blur', function () {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-})
-
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function () {
     if (window.scrollY > 500) {
+        // 배지 숨기기
         //gsap.to(요소,지속시간,옵션)
         gsap.to(badgeEl, .6, {
             opacity: 0,
             display: 'none'
         });
+        //버튼 보이기
+        gsap.to(toTopEl,.2,{
+            x: 0
+        });
     } else {
+        //배지 보이기
         gsap.to(badgeEl, .6, {
             opacity: 1,
             display: 'block'
         })
+        //버튼 숨기기
+        gsap.to(toTopEl,.2,{
+            x: 100
+        });
     }
-
 }, 300));
+//_.throttle(함수,시간)
 
+toTopEl.addEventListener('click', function(){
+    gsap.to(window,.7,{
+        scrollTo : 0
+    });
+})
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
@@ -131,6 +130,4 @@ spyEls.forEach(function(spyEl){
     .addTo(new ScrollMagic.Controller());
 })
 
-  const thisYear = document.querySelector('.this-year');
-  thisYear.textContent = new Date().getFullYear() + " Starbucks Coffe compay. All Rights Reserved";
   
